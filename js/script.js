7d0bd963544f183c352a376ab93345d774301fdc
michael <mikelkarije@gmail.com>
2019-07-29 14:27:54 +0300

    add channges to my cart

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // ("#cart-img").click(function(){
+    //     ("#cart-container").show();
+    // });
+    $("#small").submit(function(){
+        event.preventDefault();
+        
+        var quantity =$(".qty").val()
+        alert("text")
+    })
+});
