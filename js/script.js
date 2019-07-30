e964f9293788bd388e32a63fcbd3b73d152d32fa
michael <mikelkarije@gmail.com>
2019-07-30 09:53:55 +0300

    add changes to list

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // ("#cart-img").click(function(){
     //     ("#cart-container").show();
     // });
-    $("#small").submit(function(){
+    $("#small").submit(function () {
         event.preventDefault();
-        
-        var quantity =$(".qty").val()
+
+        var quantity = $(".qty").val()
         alert(quantity)
     })
+
+    $("#card-img2").click(function () {
+        event.preventDefault();
+        alert("Thank you for visiting our pizza place please come again!!");
+
+    });
+    // $("#card-img").submit(function(){
+    // <input type="button" onclick="alert('Hello World!')" value="Click Me!"></input>
+
 });
-$("#card-img").submit(function(){
-<input type="button" onclick="alert('Hello World!')" value="Click Me!"></input>
-    
-)}
